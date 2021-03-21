29cb5b000a2db569de0b9d7cc96b9d905afa3659
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-03-21 09:48:19 +0100

    added ui packages

--- app/src/main/java/com/example/patron_a_tive/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/patron_a_tive/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.patron_a_tive
+package com.example.patron_a_tive.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.patron_a_tive.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
