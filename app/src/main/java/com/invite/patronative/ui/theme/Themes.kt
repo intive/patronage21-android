eea4dcb073bde19bf9af44fbe3655385972cac37
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-03-31 10:59:55 +0200

    - colors.xml unused colors removed - Themes.kt refactored

--- app/src/main/java/com/invite/patronative/ui/theme/Themes.kt
+++ app/src/main/java/com/invite/patronative/ui/theme/Themes.kt
@@ -7,42 +7,43 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.example.patron_a_tive.R
 
-private val light_blue = Color(0xFF52BCFF)
-private val light_blue600 = Color(0xFF3FB2FA)
-private val light_blue700 = Color(0xFF29A8F8)
-private val light_blue800 = Color(0xFF11A3FF)
+private val pink300 = Color(R.color.pink_300)
+private val pink700 = Color(R.color.pink700)
 
-private val Blue200 = Color(0xFF9DA3FA)
-private val Blue400 = Color(0xFF4860F7)
-private val moderate_pink = Color(0xFFCC4C80)
-private val moderate_dark_pink = Color(0xFFC22D6A)
+private val light_blue400 = Color(R.color.light_blue400)
+private val light_blue700 = Color(R.color.light_blue700)
 
-private val Red300 = Color(0xFFEA6D7E)
-private val Red800 = Color(0xFFD00036)
+private val deep_purple300 = Color(R.color.deep_purple_300)
+private val deep_purple700 = Color(R.color.deep_purple_700)
+
+private val red800 = Color(R.color.red800)
+private val red900 = Color(R.color.red900)
 
 private val PatronativeDarkPalette = darkColors(
-    primary = Blue200,
-    primaryVariant = Blue400,
-    onPrimary = Color.Black,
-    secondary = light_blue,
+    primary = pink300,
+    primaryVariant = pink700,
+    onPrimary = Color.White,
+    secondary = light_blue400,
+    secondaryVariant = light_blue700,
     onSecondary = Color.Black,
     onSurface = Color.White,
     onBackground = Color.White,
-    error = Red300,
+    error = red800,
     onError = Color.Black
 )
 
 private val PatronativeLightPalette = lightColors(
-    primary = moderate_pink,
-    primaryVariant = moderate_dark_pink,
+    primary = deep_purple300,
+    primaryVariant = deep_purple700,
     onPrimary = Color.White,
-    secondary = light_blue700,
-    secondaryVariant = light_blue800,
+    secondary = light_blue400,
+    secondaryVariant = light_blue700,
     onSecondary = Color.Black,
-    onSurface = Color.Black,
-    onBackground = Color.Black,
-    error = Red800,
+    onSurface = Color.White,
+    onBackground = Color.White,
+    error = red900,
     onError = Color.White
 )
 
