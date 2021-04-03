bc59e830bf3142800323bf8662fff07bb638df12
Baron Harkonnen <jiklyszczarz@gmail.com>
2021-04-03 21:28:46 +0200

    Refactor CalendarHomeFragment.kt

---
+++ app/src/main/java/com/example/patron_a_tive/calendar_module/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.patron_a_tive.calendar_module.utils
+
+val calendarHeader = listOf("Pn", "Wt", "Śr", "Cz", "Pt", "Sb", "Nd")
+val weekDays = listOf("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")
--- app/src/main/java/com/example/patron_a_tive/calendar_module/viewmodels/CalendarHomeViewModel.kt
+++ app/src/main/java/com/example/patron_a_tive/calendar_module/viewmodels/CalendarHomeViewModel.kt
@@ -3,48 +3,44 @@ package com.example.patron_a_tive.calendar_module.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.patron_a_tive.calendar_module.utils.calendarHeader
 import java.util.*
 
 class CalendarHomeViewModel : ViewModel() {
-    private val calendarHeader = listOf("Pn", "Wt", "Śr", "Cz", "Pt", "Sb", "Nd")
-
-    private val _currentWeek =
-        MutableLiveData<Array<Calendar>>(getCurrentWeek(Calendar.getInstance()))
+    private val _currentWeek = MutableLiveData(getCurrentWeek(Calendar.getInstance()))
     val currentWeek: LiveData<Array<Calendar>> = _currentWeek
 
-    private val _month = MutableLiveData<Int>(Calendar.getInstance()[Calendar.MONTH])
+    private val _month = MutableLiveData(Calendar.getInstance()[Calendar.MONTH])
     val month: LiveData<Int> = _month
 
-    private val _year = MutableLiveData<Int>(Calendar.getInstance()[Calendar.YEAR])
+    private val _year = MutableLiveData(Calendar.getInstance()[Calendar.YEAR])
     val year: LiveData<Int> = _year
 
     private val currentDate: Calendar = Calendar.getInstance()
 
-
-    private val _currentMonth = MutableLiveData<List<String>>(getCurrentMonth(getFirstDay()))
+    private val _currentMonth = MutableLiveData(getCurrentMonth(getFirstDay()))
     var currentMonth: LiveData<List<String>> = _currentMonth
 
-    private val _showPeriodDialog = MutableLiveData<Boolean>(false)
+    private val _showPeriodDialog = MutableLiveData(false)
     val showPeriodDialog: LiveData<Boolean> = _showPeriodDialog
 
-    private val _showWeekView = MutableLiveData<Boolean>(true)
+    private val _showWeekView = MutableLiveData(true)
     val showWeekView: LiveData<Boolean> = _showWeekView
 
-
-    private val _bColorWeekBtn = MutableLiveData<Long>(0xff52bcff)
+    private val _bColorWeekBtn = MutableLiveData(0xff52bcff)
     val bColorWeekBtn: LiveData<Long> = _bColorWeekBtn
 
-    private val _bColorMonthBtn = MutableLiveData<Long>(0xffffffff)
+    private val _bColorMonthBtn = MutableLiveData(0xffffffff)
     val bColorMonthBtn: LiveData<Long> = _bColorMonthBtn
 
-    private val _txtColorWeekBtn = MutableLiveData<Long>(0xffffffff)
+    private val _txtColorWeekBtn = MutableLiveData(0xffffffff)
     val txtColorWeekBtn: LiveData<Long> = _txtColorWeekBtn
 
-    private val _txtColorMonthBtn = MutableLiveData<Long>(0xff000000)
+    private val _txtColorMonthBtn = MutableLiveData(0xff000000)
     val txtColorMonthBtn: LiveData<Long> = _txtColorMonthBtn
 
 
-    fun getCurrentWeek(date: Calendar): Array<Calendar>? {
+    private fun getCurrentWeek(date: Calendar): Array<Calendar> {
         val dayOfTheWeek = date[Calendar.DAY_OF_WEEK]
 
         when {
@@ -98,22 +94,14 @@ class CalendarHomeViewModel : ViewModel() {
         _showWeekView.value = false
     }
 
-    fun setMonth(value: Int) {
-        _month.value = value
-    }
 
-    fun setYear(value: Int) {
-        _year.value = value
-    }
-
-
-    private fun getCurrentMonth(firstDay: Calendar): List<String>? {
+    private fun getCurrentMonth(firstDay: Calendar): List<String> {
 
         val dayOfTheWeek = firstDay[Calendar.DAY_OF_WEEK]
         val daysNumber = firstDay.getActualMaximum(Calendar.DAY_OF_MONTH)
 
 
-        var offset: Int = when {
+        val offset: Int = when {
             dayOfTheWeek == 2 -> {
                 0
             }
@@ -125,7 +113,7 @@ class CalendarHomeViewModel : ViewModel() {
             }
         }
 
-        var offsetList = listOf<String>()
+        val offsetList = mutableListOf<String>()
         val numbers = (1..daysNumber).toList().map { it.toString() }
 
         repeat(offset) {
@@ -153,21 +141,21 @@ class CalendarHomeViewModel : ViewModel() {
     }
 
     private fun getFirstDay(): Calendar {
-        var date = Calendar.getInstance()
+        val date = Calendar.getInstance()
         _month.value?.let { date.set(Calendar.MONTH, it) }
         date.set(Calendar.DATE, 1)
         _year.value?.let { date.set(Calendar.YEAR, it) }
         return date
     }
 
-    fun weekClicked(){
+    fun weekClicked() {
         _bColorWeekBtn.value = 0xff52bcff
         _bColorMonthBtn.value = 0xffffffff
         _txtColorWeekBtn.value = 0xffffffff
         _txtColorMonthBtn.value = 0xff000000
     }
 
-    fun monthClicked(){
+    fun monthClicked() {
         _bColorMonthBtn.value = 0xff52bcff
         _bColorWeekBtn.value = 0xffffffff
         _txtColorMonthBtn.value = 0xffffffff
