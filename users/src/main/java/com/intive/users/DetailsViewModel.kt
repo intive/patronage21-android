ced68f0d6e1caf4beb753ba54a262b3623f7e293
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-04-07 13:33:40 +0200

    added Person object fields

--- users/src/main/java/com/intive/users/DetailsViewModel.kt
+++ users/src/main/java/com/intive/users/DetailsViewModel.kt
@@ -5,7 +5,15 @@ import androidx.lifecycle.ViewModel
 class DetailsViewModel : ViewModel(){
     data class Project(val name: String, val role: String)
 
-    val user = Person("Jan", "Kowalski")
+    val user = Person(
+        "Jan",
+        "Kowalski",
+        "jankowalski@gmal.com",
+        "123456789",
+        "github.com/KowalskiJan",
+        "Jestem programista"
+    )
+
     val projects = listOf(
         Project("Projekt I", "Scrum Master"),
         Project("Projekt II", "Product Owner"),
