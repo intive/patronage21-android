07b6e07716198405d5bd0dd160767885a625ceae
masslany <p.atryk@outlook.com>
2021-04-13 16:41:13 +0200

    Remove unused class

--- users/src/main/java/com/intive/users/presentation/edit_user/EditUserViewModel.kt
+++ users/src/main/java/com/intive/users/presentation/edit_user/EditUserViewModel.kt
@@ -2,18 +2,7 @@ package com.intive.users.presentation.edit_user
 
 import androidx.lifecycle.ViewModel
 
-data class UserDataCredentials(
-    var gender: String,
-    var firstName: String,
-    var lastName: String,
-    var email: String,
-    var phoneNumber: String,
-    var github: String,
-    var bio: String
-)
 
-class EditUserViewModel : ViewModel(){
-
-
+class EditUserViewModel : ViewModel() {
 
 }
