372be2b19ea26a99b9a84f2ae11adec2b06d9cab
masslany <p.atryk@outlook.com>
2021-04-16 20:32:16 +0200

    Add Koin support and fetch users from API

--- users/src/main/java/com/intive/users/presentation/users/UsersViewModel.kt
+++ users/src/main/java/com/intive/users/presentation/users/UsersViewModel.kt
@@ -4,9 +4,15 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.intive.repository.Repository
 import com.intive.users.domain.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class UsersViewModel : ViewModel() {
+class UsersViewModel(
+    private val repository: Repository
+) : ViewModel() {
     val users = List(5) {
         User(
             firstName = "Jan",
@@ -17,6 +23,17 @@ class UsersViewModel : ViewModel() {
             github = "github.com/KowalskiJan",
             bio = "Jestem programista"
         )
+    }
+
+
+    init {
+        viewModelScope.launch(Dispatchers.IO){
+            try{
+                println("USERS: " + repository.getUsers())
+            } catch (e: Exception) {
+                println("EXCEPTION VM: " + e.localizedMessage)
+            }
+        }
     }
 
     private val _query: MutableState<String> = mutableStateOf("")
