fe64fadecef1da91412c8e8ce802593b892bf7f4
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-04-28 12:49:13 +0200

    implemented PagingSource

--- repository/src/main/java/com/intive/repository/Repository.kt
+++ repository/src/main/java/com/intive/repository/Repository.kt
@@ -3,8 +3,10 @@ package com.intive.repository
 import com.intive.repository.domain.model.Audit
 import com.intive.repository.domain.model.User
 import com.intive.repository.network.response.AuditResponse
+import com.intive.repository.network.util.AuditDtoMapper
 
 interface Repository {
+    val auditsMapper: AuditDtoMapper
     suspend fun getUsers(): List<User>
 
     suspend fun getTechnologyGroups(): List<String>
--- repository/src/main/java/com/intive/repository/network/AuditsSource.kt
+++ repository/src/main/java/com/intive/repository/network/AuditsSource.kt
@@ -1,24 +1,28 @@
 package com.intive.repository.network
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.intive.repository.Repository
+import com.intive.repository.domain.model.Audit
 import com.intive.repository.domain.model.User
 
 const val AUDITS_STARTING_PAGE_INDEX = 1
 
 class AuditsSource(
     private val repository: Repository,
-) : PagingSource<Int, User>() {
+) : PagingSource<Int, Audit>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, User> {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Audit> {
         return try {
             val page = params.key ?: AUDITS_STARTING_PAGE_INDEX
             val auditResponse = repository.searchAudits(page = page, query = "")
             val audits = auditResponse.audits
 
             LoadResult.Page(
-                data = repository.usersMapper.mapToDomainList(users),
+                data = repository.auditsMapper.toDomainList(audits),
                 prevKey = auditResponse.previousPage,
                 nextKey = auditResponse.nextPage
             )
@@ -27,5 +31,5 @@ class AuditsSource(
         }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, User>): Int? = state.anchorPosition
+    override fun getRefreshKey(state: PagingState<Int, Audit>): Int? = state.anchorPosition
 }
