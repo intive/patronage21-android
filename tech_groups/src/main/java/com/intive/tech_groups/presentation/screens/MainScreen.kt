62b82b661e55515060ff7f7766c511406a9255a7
Bartekk <bartekturkosz@gmail.com>
2021-04-29 23:28:14 +0200

    create tech_groups module

---
+++ tech_groups/src/main/java/com/intive/tech_groups/presentation/screens/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.intive.tech_groups.presentation.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text("tutaj będzie ekran główny :)")
+}
