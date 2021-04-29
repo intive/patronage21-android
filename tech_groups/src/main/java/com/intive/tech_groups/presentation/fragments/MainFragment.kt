7ce062a40788b7ad088adfbcd00480938391b5b1
Bartekk <bartekturkosz@gmail.com>
2021-04-30 01:13:36 +0200

    create tech groups screen

--- tech_groups/src/main/java/com/intive/tech_groups/presentation/fragments/MainFragment.kt
+++ tech_groups/src/main/java/com/intive/tech_groups/presentation/fragments/MainFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.intive.tech_groups.presentation.screens.MainScreen
+import com.intive.tech_groups.presentation.viewmodels.MainViewModel
 import com.intive.ui.PatronativeTheme
 
 
 class MainFragment : Fragment() {
 
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,7 +22,7 @@ class MainFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 PatronativeTheme {
-                    MainScreen()
+                    MainScreen(viewModel)
                 }
             }
         }
--- tech_groups/src/main/java/com/intive/tech_groups/presentation/screens/MainScreen.kt
+++ tech_groups/src/main/java/com/intive/tech_groups/presentation/screens/MainScreen.kt
@@ -1,9 +1,137 @@
 package com.intive.tech_groups.presentation.screens
 
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.intive.tech_groups.R
+import com.intive.tech_groups.presentation.viewmodels.MainViewModel
+import com.intive.ui.PatronageTypography
+import com.intive.ui.components.Spinner
+import com.intive.ui.components.TitleText
 
 @Composable
-fun MainScreen() {
-    Text("tutaj będzie ekran główny :)")
+fun MainScreen(viewModel: MainViewModel) {
+    val filteredList by viewModel.filteredList.observeAsState()
+
+    Column() {
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .padding(
+                    start = dimensionResource(id = R.dimen.screen_padding),
+                    end = dimensionResource(id = R.dimen.screen_padding),
+                )
+                .fillMaxWidth()
+                .verticalScroll(scrollState),
+        ) {
+            TitleText(
+                text = stringResource(R.string.tech_groups_title),
+                style = MaterialTheme.typography.h5,
+                color = MaterialTheme.colors.secondary,
+                modifier = Modifier
+                    .padding(top = 15.dp, bottom = 15.dp)
+            )
+            Text(
+                text = "tutaj będzie ekran główny :)",
+                style = PatronageTypography.body2,
+                modifier = Modifier
+                    .padding(top = 15.dp, bottom = 15.dp)
+            )
+            Spinner(items = viewModel.filters) {
+                viewModel.filterList(it)
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                var index = 0
+                while (!filteredList.isNullOrEmpty() && index < filteredList!!.size) {
+                    Row {
+                        Column(Modifier.weight(1f)) {
+                            BoxButton(
+                                text = filteredList!![index].name,
+                                onClick = { },
+                            ) {
+                                Text("Technologie: ")
+                                for (technology in filteredList!![index].techList) {
+                                    Text(technology)
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.size(20.dp))
+                        Column(Modifier.weight(1f)) {
+                            if (index + 1 < filteredList!!.size) {
+                                BoxButton(
+                                    text = filteredList!![index + 1].name,
+                                    onClick = { }
+                                ) {
+                                    Text("Technologie: ")
+                                    for (technology in filteredList!![index + 1].techList) {
+                                        Text(technology)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(20.dp))
+                    index += 2
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BoxButton(
+    text: String,
+    borderColor: Color = MaterialTheme.colors.primary,
+    onClick: (() -> Unit) = {},
+    content: @Composable () -> Unit
+) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .aspectRatio(1f)
+        .border(
+            border = BorderStroke(1.dp, color = borderColor),
+            shape = RoundedCornerShape(20.dp)
+        )
+        .clip(shape = RoundedCornerShape(20.dp))
+        .clickable { onClick() }) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .aspectRatio(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = text,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                style = PatronageTypography.body2,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            content()
+        }
+    }
 }
