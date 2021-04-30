15e8d144f03330118e4f78c649f76a1334a50f11
Bartekk <bartekturkosz@gmail.com>
2021-04-30 14:20:30 +0200

    refactor and add new endpoint for techGroups

--- repository/src/main/java/com/intive/repository/network/TechnologiesService.kt
+++
@@ -1,8 +0,0 @@
-package com.intive.repository.network
-
-import retrofit2.http.GET
-
-interface TechnologiesService {
-    @GET("api/groups")
-    suspend fun getTechnologies(): List<String>
-}
---
+++ repository/src/main/java/com/intive/repository/network/TechnologyGroupsService.kt
@@ -0,0 +1,13 @@
+package com.intive.repository.network
+
+import com.intive.repository.domain.model.Group
+import retrofit2.http.GET
+
+interface TechnologyGroupsService {
+
+    @GET("api/groups/technologies")
+    suspend fun getTechnologies(): List<String>
+
+    @GET("api/groups")
+    suspend fun getTechnologyGroups(): List<Group>
+}
