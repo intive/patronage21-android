3e01c1babb3a4f943d2b1624b1c440e9465f49da
Joanna Lyszczarz <jiklyszczarz@gmail.com>
2021-05-06 18:31:58 +0200

    Add layout container

--- ui/src/main/java/com/intive/ui/components/IntroSection.kt
+++ ui/src/main/java/com/intive/ui/components/IntroSection.kt
@@ -1,11 +1,8 @@
 package com.intive.ui.components
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
