5a04ae88aa316b5ba891e6079a1289e872f65d40
masslany <p.atryk@outlook.com>
2021-05-13 21:28:08 +0200

    Handle user contact actions

--- users/src/main/java/com/intive/users/presentation/details/DetailsFragment.kt
+++ users/src/main/java/com/intive/users/presentation/details/DetailsFragment.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.intive.ui.PatronativeTheme
 import com.intive.users.presentation.composables.screens.DetailsScreen
+import kotlinx.coroutines.flow.collect
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import android.content.Intent
+import android.net.Uri
+
 
 class DetailsFragment : Fragment() {
 
-    private val viewModel: DetailsViewModel by viewModels()
+    private val viewModel: DetailsViewModel by viewModel<DetailsViewModel>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,8 +35,42 @@ class DetailsFragment : Fragment() {
                     DetailsScreen(
                         navController = navController,
                         user = mock,
-                        projects = projects
+                        projects = projects,
+                        viewModel = viewModel
                     )
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.userContactEvent.collect { event ->
+                when(event) {
+                    is DetailsViewModel.UserContactEvent.DialPhoneNumber -> {
+                        val phoneNumber = event.phoneNumber
+
+                        val intent =
+                            Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                        startActivity(intent)
+                    }
+                    is DetailsViewModel.UserContactEvent.LaunchWebsite -> {
+                        var websiteUrl = event.websiteUrl
+                        if (!websiteUrl.startsWith("http://") && !websiteUrl.startsWith("https://"))
+                            websiteUrl = "http://$websiteUrl"
+
+                        val intent =
+                            Intent(Intent.ACTION_VIEW, Uri.parse(websiteUrl))
+                        startActivity(intent)
+                    }
+                    is DetailsViewModel.UserContactEvent.SendEmail -> {
+                        val email = event.email
+
+                        val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$email"))
+                        startActivity(intent)
+                    }
                 }
             }
         }
