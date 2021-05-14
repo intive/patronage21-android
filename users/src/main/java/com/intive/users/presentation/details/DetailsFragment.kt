d7d4639940b20e92219a7ac1c51e121fe82d0e95
masslany <p.atryk@outlook.com>
2021-05-14 22:10:24 +0200

    Create deeplink with login argument to user details

--- users/src/main/java/com/intive/users/presentation/details/DetailsFragment.kt
+++ users/src/main/java/com/intive/users/presentation/details/DetailsFragment.kt
@@ -4,21 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.intive.ui.PatronativeTheme
 import com.intive.users.presentation.composables.screens.DetailsScreen
 
 class DetailsFragment : Fragment() {
 
     private val viewModel: DetailsViewModel by viewModels()
+    private val args: DetailsFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val mock = viewModel.user
+        mock.firstName = args.login
         val projects = viewModel.projects
 
         val navController = findNavController()
