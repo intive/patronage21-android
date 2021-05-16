8148a13aafc45d9a26c0c45375775f48315664ee
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-05-16 14:45:58 +0200

    changed LayoutContainer padding to dimensionResource

--- ui/src/main/java/com/intive/ui/components/LayoutContainer.kt
+++ ui/src/main/java/com/intive/ui/components/LayoutContainer.kt
@@ -3,15 +3,15 @@ package com.intive.ui.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.res.dimensionResource
+import com.intive.ui.R
 
 @Composable
 fun LayoutContainer(content: @Composable () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp)
+            .padding(dimensionResource(id = R.dimen.screen_padding))
     ) {
         content()
     }
