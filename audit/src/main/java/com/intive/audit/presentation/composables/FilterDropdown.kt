b9064a6c1b9590a3f5aab38c65748efc08df88ee
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-05-21 13:37:05 +0200

    Audit filters wokring, common UI component changed

--- audit/src/main/java/com/intive/audit/presentation/composables/FilterDropdown.kt
+++ audit/src/main/java/com/intive/audit/presentation/composables/FilterDropdown.kt
@@ -1,59 +1,45 @@
 package com.intive.audit.presentation.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Done
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.intive.audit.R
+import com.intive.ui.components.DropDownList
 
 @Composable
-fun FilterDropdown(expanded: Boolean, updateExpand: (Boolean) -> Unit) {
-    val items = listOf("Od najnowszych", "Od najstarszych")
-    var selectedIndex by remember { mutableStateOf(0) }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.TopEnd)
-            .padding(top = 50.dp)
-    ) {
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { updateExpand(false) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    Color.White
-                )
-        ) {
-            items.forEachIndexed { index, s ->
-                DropdownMenuItem(
-                    onClick = {
-                        selectedIndex = index
-                        updateExpand(false)
-                    },
-                )
-                {
-                    Text(
-                        text = s,
-                        modifier = Modifier.weight(1f)
-                    )
-                    if (index == selectedIndex) {
-                        Icon(
-                            Icons.Outlined.Done,
-                            contentDescription = stringResource(R.string.arrow_upward_icon_desc)
-                        )
-                    }
+fun SpinnerFilter(
+    items: List<String>,
+    expanded: Boolean,
+    updateExpand: (Boolean) -> Unit,
+    onTitleSelected: (String) -> Unit,
+) {
+    val text = rememberSaveable { mutableStateOf(items[0]) }
+    val isOpen = rememberSaveable { mutableStateOf(false) }
+    Box {
+        Column {
+            DropDownList(
+                requestToOpen = expanded,
+                dropDownIconShow = true,
+                list = items,
+                updateExpand,
+                selectedString = {
+                    onTitleSelected(it)
+                    text.value = it
                 }
-                if (index != items.size - 1)
-                    Divider(color = Color.LightGray)
-            }
+            )
         }
+        Spacer(
+            modifier = Modifier
+                .matchParentSize()
+                .background(Color.Transparent)
+                .padding(top = 10.dp)
+                .clickable(
+                    onClick = { isOpen.value = true }
+                )
+        )
     }
 }
