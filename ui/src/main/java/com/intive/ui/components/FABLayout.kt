1f3b625c86f3209ffe2bfe7cacff753e5637f37c
Joanna Lyszczarz <jiklyszczarz@gmail.com>
2021-05-25 21:01:28 +0200

    Remove unused import

--- ui/src/main/java/com/intive/ui/components/FABLayout.kt
+++ ui/src/main/java/com/intive/ui/components/FABLayout.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
 import com.intive.ui.R
 
 @Composable
