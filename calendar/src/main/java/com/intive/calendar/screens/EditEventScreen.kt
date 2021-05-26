a385fb8128105aad7cc346e94d6bf22eb92867c3
Joanna Lyszczarz <jiklyszczarz@gmail.com>
2021-05-26 14:13:57 +0200

    Refactor

--- calendar/src/main/java/com/intive/calendar/screens/EditEventScreen.kt
+++ calendar/src/main/java/com/intive/calendar/screens/EditEventScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.res.stringResource
 import com.intive.calendar.R
 import com.intive.calendar.viewmodels.AddEventViewModel
 import com.intive.calendar.components.EventForm
+import com.intive.calendar.utils.extractTimeFromString
 import com.intive.shared.EventParcelable
 import com.intive.shared.stringToCalendar
 
@@ -20,14 +21,12 @@ fun EditEventScreen(
     refreshEventsList: () -> Unit
 ) {
 
+    val time = extractTimeFromString(event.time)
+
     addEventViewModel.setDate(stringToCalendar(event.date, "."))
     addEventViewModel.setInputValue(event.name)
-
-    val time = event.time.filterNot { it.isWhitespace() }
-    val (startTime, endTime) = time.split("-")
-
-    addEventViewModel.setTimeStart(startTime.split(":")[0].toInt(), startTime.split(":")[1].toInt())
-    addEventViewModel.setTimeEnd(endTime.split(":")[0].toInt(), endTime.split(":")[1].toInt())
+    addEventViewModel.setTimeStart(time[0], time[1])
+    addEventViewModel.setTimeEnd(time[2], time[3])
 
     EventForm(
         titleText = stringResource(R.string.edit_event_header),
