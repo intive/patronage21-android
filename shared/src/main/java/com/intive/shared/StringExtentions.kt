a113bbae74867b7323f514e132987489608a2c84
masslany <p.atryk@outlook.com>
2021-06-03 23:04:26 +0200

    Remove unnecessary println()

--- shared/src/main/java/com/intive/shared/StringExtentions.kt
+++ shared/src/main/java/com/intive/shared/StringExtentions.kt
@@ -5,9 +5,7 @@ import android.graphics.BitmapFactory
 import android.util.Base64
 
 fun String.decodeBase64IntoBitmap(): Bitmap {
-    println(this)
     val imageBytes = Base64.decode(this, Base64.DEFAULT)
-    println(imageBytes)
     return BitmapFactory.decodeByteArray(
         imageBytes, 0, imageBytes.size)
 }
