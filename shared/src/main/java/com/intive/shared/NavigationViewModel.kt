f43ebccdac72699480934ee9e272126c996dfd8b
KubinkiewiczDawid <dawi.kubinkiewicz@gmail.com>
2021-06-15 15:00:31 +0200

    User Icon showing only when loged In

--- shared/src/main/java/com/intive/shared/NavigationViewModel.kt
+++ shared/src/main/java/com/intive/shared/NavigationViewModel.kt
@@ -1,6 +1,9 @@
 package com.intive.shared
 
 import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -18,12 +21,17 @@ class NavigationViewModel(
     private val _loggedInChannel = Channel<LoginEvent>()
     val loginFlow = _loggedInChannel.receiveAsFlow()
 
+    private val _loggedState: MutableState<LoginEvent> = mutableStateOf(LoginEvent.UserLoggedIn)
+    val loggedState: State<LoginEvent> = _loggedState
+
     init {
         viewModelScope.launch {
             if(repository.isUserLogged()){
                 _loggedInChannel.send(LoginEvent.UserLoggedIn)
+                _loggedState.value = LoginEvent.UserLoggedIn
             } else {
                 _loggedInChannel.send(LoginEvent.UserLoggedOut)
+                _loggedState.value = LoginEvent.UserLoggedOut
             }
         }
     }
@@ -32,12 +40,14 @@ class NavigationViewModel(
         repository.loginUser(login)
         viewModelScope.launch {
             _loggedInChannel.send(LoginEvent.UserLoggedIn)
+            _loggedState.value = LoginEvent.UserLoggedIn
         }
     }
 
     fun logoutUser() {
         viewModelScope.launch {
             _loggedInChannel.send(LoginEvent.UserLoggedOut)
+            _loggedState.value = LoginEvent.UserLoggedOut
         }
     }
 
